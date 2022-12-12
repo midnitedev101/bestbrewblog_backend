16fab52c275a69bf707cbbae2e456fb8353eccd7
QwirkyRabbit <26995475+midnitedev101@users.noreply.github.com>
2022-12-11 17:21:05 -0800

    Create new child theme and update layout changes

---
+++ public/.wpengine-conf/wp-config-site.php
@@ -0,0 +1,5 @@
+<?php
+# GENERATED AUTOMATICALLY ON 2022-11-20 23:32:00.454673 -0800 PST m=+662.403210501
+$table_prefix = "wp_";
+$wpengine_primary_original_domain = "bestbrewblog.wpengine.com";
+$wpengine_primary_original_ip_address = "34.83.40.209";
